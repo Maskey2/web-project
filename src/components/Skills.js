21e89b0720d7cf377007138b8c09a85eabef7865
SeaLand <sldrigging.com>
2020-09-20 15:56:51 -0500

    updates

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { makeStyles } from '@material-ui/core/styles';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+function ElevationScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  });
 
-export default function Skills() {
-    return (
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }));
+ElevationScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default function ElevateAppBar(props) {
+    const classes = useStyles();
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <ElevationScroll {...props}>
+        <AppBar style={{backgroundColor:'black'}}>
+          <Toolbar>
+            <Typography variant="h6"><ArrowBackIosIcon style={{verticalAlign:'middle'}}/> Dashboard</Typography>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <Toolbar />
+      <Container>
+      <Grid container spacing={3}>
+        
+        <Grid item xs={6}>
+          <div>Image</div>
+        </Grid>
+        <Grid item xs={6}>
         <div>
-            Skills
-        </div>
-    )
+              <h2>About Me</h2>
+              </div>
+        </Grid>
+        
+        </Grid>
+      </Container>
+    </React.Fragment>
+  );
 }
